fc13d1f4c26cef66a730c9c0f9731805a66f34c8
Jort Driegen <39172426+jortdr@users.noreply.github.com>
2024-03-12 09:23:17 +0100

    added inventory presenter labels

--- Assets/Scripts/Presenters/InventoryPresenter.cs
+++ Assets/Scripts/Presenters/InventoryPresenter.cs
@@ -5,11 +5,24 @@ using UnityEngine;
 
 namespace Presenters
 {
-    public class InventoryPresenter: MonoBehaviour
+    public class InventoryPresenter : MonoBehaviour
     {
-        [SerializeField] private TMP_Text player;
+        [Header("Tribe debug info")] [SerializeField]
+        private TMP_Text player;
+
         [SerializeField] private TMP_Text cpu1;
         [SerializeField] private TMP_Text cpu2;
+
+        [Header("Inventory")] [SerializeField] private TMP_Text inventoryWood;
+        [SerializeField] private TMP_Text inventoryLenses;
+        [SerializeField] private TMP_Text inventoryClay;
+        [SerializeField] private TMP_Text inventoryGold;
+        [SerializeField] private TMP_Text inventorySteel;
+        [SerializeField] private TMP_Text inventoryInsulation;
+        [SerializeField] private TMP_Text inventoryFertilizer;
+        [SerializeField] private TMP_Text inventoryStone;
+
+
         private GameManager _gameManager;
 
         private void Start()
@@ -18,17 +31,38 @@ namespace Presenters
             player.text = _gameManager.Player.Inventory.ToString();
             cpu1.text = _gameManager.Cpu1.Inventory.ToString();
             cpu2.text = _gameManager.Cpu2.Inventory.ToString();
-            
+
             _gameManager.Player.Inventory.InventoryUpdate += OnInventoryUpdate;
             _gameManager.Cpu1.Inventory.InventoryUpdate += OnInventoryUpdate;
             _gameManager.Cpu2.Inventory.InventoryUpdate += OnInventoryUpdate;
+            
+            UpdateInventoryLabels();
         }
-        
+
         private void OnInventoryUpdate(object sender, EventArgs eventArgs)
         {
             player.text = _gameManager.Player.Inventory.ToString();
             cpu1.text = _gameManager.Cpu1.Inventory.ToString();
             cpu2.text = _gameManager.Cpu2.Inventory.ToString();
+            
+            UpdateInventoryLabels();
+        }
+
+        private void UpdateInventoryLabels()
+        {
+            setTextLabel(inventoryWood, InventoryItems.Wood);
+            setTextLabel(inventoryLenses, InventoryItems.Lenses);
+            setTextLabel(inventoryClay, InventoryItems.Clay);
+            setTextLabel(inventoryGold, InventoryItems.Gold);
+            setTextLabel(inventorySteel, InventoryItems.Steel);
+            setTextLabel(inventoryInsulation, InventoryItems.Insulation);
+            setTextLabel(inventoryFertilizer, InventoryItems.Fertilizer);
+            setTextLabel(inventoryStone, InventoryItems.Stone);
+        }
+
+        private void setTextLabel(TMP_Text label, InventoryItems item)
+        {
+            label.text = _gameManager.Player.Inventory.GetInventoryAmount(item).ToString();
         }
     }
 }
