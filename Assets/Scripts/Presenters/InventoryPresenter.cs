e38921e2270d505aee540c29726241fb03ca74c8
PatrickSlag <patrickslag@gmail.com>
2024-03-26 10:51:16 +0100

    NT-136 inventory linked to ui
    update ui numbers
    animation

--- Assets/Scripts/Presenters/InventoryPresenter.cs
+++ Assets/Scripts/Presenters/InventoryPresenter.cs
@@ -1,34 +1,81 @@
 using System;
 using Enums;
+using Models;
 using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 namespace Presenters
 {
     public class InventoryPresenter: MonoBehaviour
     {
-        [SerializeField] private TMP_Text player;
-        [SerializeField] private TMP_Text cpu1;
-        [SerializeField] private TMP_Text cpu2;
+        [SerializeField] private Button resourceCardButton;
+        [SerializeField] private Transform resourceCard;
+        [SerializeField] private Transform resourceCardTarget;
+        [SerializeField] private float speed;
+        private Vector3 _originalPosition;
+        private Vector3 _targetPosition;
+
+        [SerializeField] private TMP_Text wood;
+        [SerializeField] private TMP_Text insulation;
+        [SerializeField] private TMP_Text stone;
+        [SerializeField] private TMP_Text fertilizer;
+        [SerializeField] private TMP_Text lenses;
+        [SerializeField] private TMP_Text clay;
+        [SerializeField] private TMP_Text gold;
+        [SerializeField] private TMP_Text steel;
+
         private GameManager _gameManager;
+        private Tribe _player;
+        private bool _transitioning = false;
 
         private void Start()
         {
             _gameManager = GameManager.Instance;
-            player.text = _gameManager.Player.Inventory.ToString();
-            cpu1.text = _gameManager.Cpu1.Inventory.ToString();
-            cpu2.text = _gameManager.Cpu2.Inventory.ToString();
+            _player = _gameManager.Player;
             
-            _gameManager.Player.Inventory.InventoryUpdate += OnInventoryUpdate;
-            _gameManager.Cpu1.Inventory.InventoryUpdate += OnInventoryUpdate;
-            _gameManager.Cpu2.Inventory.InventoryUpdate += OnInventoryUpdate;
+            resourceCardButton.onClick.AddListener(ToggleResourceCard);
+            _originalPosition = resourceCard.position;
+            _targetPosition = _originalPosition;
+            
+            _player.Inventory.InventoryUpdate += OnInventoryUpdate;
+            _player.Inventory.UpdateInventory();
+        }
+
+        private void ToggleResourceCard()
+        {
+            if(_transitioning) return;
+            if (_targetPosition == _originalPosition)
+            {
+                _targetPosition = resourceCardTarget.position;
+                _transitioning = true;
+            }
+            else
+            {
+                _targetPosition = _originalPosition;
+                _transitioning = true;
+            }
         }
         
         private void OnInventoryUpdate(object sender, EventArgs eventArgs)
         {
-            player.text = _gameManager.Player.Inventory.ToString();
-            cpu1.text = _gameManager.Cpu1.Inventory.ToString();
-            cpu2.text = _gameManager.Cpu2.Inventory.ToString();
+            wood.text = _player.Inventory.GetInventoryAmount(InventoryItems.Wood).ToString();
+            insulation.text = _player.Inventory.GetInventoryAmount(InventoryItems.Insulation).ToString();
+            stone.text = _player.Inventory.GetInventoryAmount(InventoryItems.Stone).ToString();
+            fertilizer.text = _player.Inventory.GetInventoryAmount(InventoryItems.Fertilizer).ToString();
+            lenses.text = _player.Inventory.GetInventoryAmount(InventoryItems.Lenses).ToString();
+            clay.text = _player.Inventory.GetInventoryAmount(InventoryItems.Clay).ToString();
+            gold.text = _player.Inventory.GetInventoryAmount(InventoryItems.Gold).ToString();
+            steel.text = _player.Inventory.GetInventoryAmount(InventoryItems.Steel).ToString();
+        }
+
+        private void Update()
+        {
+            if (_transitioning)
+            {
+                resourceCard.position = Vector3.MoveTowards(resourceCard.position, _targetPosition, speed);
+                if (resourceCard.position == _targetPosition) _transitioning = false;
+            }
         }
     }
 }
