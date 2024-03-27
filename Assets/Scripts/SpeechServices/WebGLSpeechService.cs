c857e91ce9549c63003d72cc9d271c415fcbc1c2
PatrickSlag <44977618+PatrickSlag@users.noreply.github.com>
2024-03-27 12:32:27 +0100

    NT-225 Sppechservice

--- Assets/Scripts/SpeechServices/WebGLSpeechService.cs
+++ Assets/Scripts/SpeechServices/WebGLSpeechService.cs
@@ -18,13 +18,20 @@ namespace SpeechServices
         private static extern void StartSpeechRecognition();
         
         [System.Runtime.InteropServices.DllImport("__Internal")]
-        private static extern void Speak();
+        private static extern void SetupTextToSpeech(int voice, int volume, int rate, int pitch, string gameObjectName, string onEndCallbackName);
+        
+        [System.Runtime.InteropServices.DllImport("__Internal")]
+        private static extern void Speak(string text);
+        
+        [System.Runtime.InteropServices.DllImport("__Internal")]
+        private static extern void StopSpeaking();
 
         private void Start()
         {
             //setup javascript code to recognize speech.
             //given are the game object and functions on that object, that should be called when speech is recognized
-            SetupSpeechRecognition("GameManager", "OnLiveSpeechTranscribe", "OnFinalSpeechRecognized");
+            SetupSpeechRecognition(gameObject.name, nameof(OnLiveSpeechTranscribe), nameof(OnFinalSpeechRecognized));
+            SetupTextToSpeech(1,1,1,1, gameObject.name, nameof(OnSpeechEnded) );
         }
 
         public void StartRecognition()
@@ -33,7 +40,11 @@ namespace SpeechServices
             {
                 StartSpeechRecognition();
             }
-            Speak();
+        }
+
+        public void SpeakText(string text)
+        {
+            Speak(text);
         }
 
         public bool CheckSupport()
@@ -51,6 +62,12 @@ namespace SpeechServices
         public void OnFinalSpeechRecognized(string text)
         {
             SpeechTranscribe?.Invoke(this, new SpeechTranscribeEventArgs{IsFinal = true, Text = text});
+        }
+
+        public void OnSpeechEnded()
+        {
+            //tts ended
+            Debug.Log("The TTS has ended");
         }
     }
 }
